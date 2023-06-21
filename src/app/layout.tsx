436c14b5306061c54a7a24a08a5e022a9c5d409d
Cristofe Albuquerque <cristofealbuquerque7@gmail.com>
2023-06-21 20:35:45 -0300

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,19 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.png" sizes="any" />
+        {/* Google tag (gtag.js) */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRAKING}`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRAKING}');
+            `
+          }}
+        />
       </head>
       <body className={`${roboto.variable} ${ibm.variable} font-sans`}> {children} </body>
     </html>
