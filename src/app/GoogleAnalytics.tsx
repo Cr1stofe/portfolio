d14fb1d06ce0f52396b42fbd1146cf3bba597088
Cristofe Albuquerque <cristofealbuquerque7@gmail.com>
2023-09-05 11:41:29 -0300

    fix: google analytics

---
+++ src/app/GoogleAnalytics.tsx
@@ -0,0 +1,25 @@
+//GoogleAnalytics.tsx
+
+"use client";
+import Script from "next/script";
+
+const GoogleAnalytics = ({ GA_TRACKING_ID }: { GA_TRACKING_ID: string }) => {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${GA_TRACKING_ID}');
+        `}
+      </Script>
+    </>
+  );
+};
+
+export default GoogleAnalytics;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import {
   Roboto_Flex as Roboto,
   IBM_Plex_Mono as IBM
 } from 'next/font/google'
-import GoogleAnalytics from '@/components/GoogleAnalytics'
+import GoogleAnalytics from './GoogleAnalytics'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 const ibm = IBM({ subsets: ['latin'], weight: '700', variable: '--font-ibm' })
@@ -22,8 +22,9 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.png" sizes="any" />
+
+        <GoogleAnalytics GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_TRAKING as string} />
       </head>
-      <GoogleAnalytics GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_TRAKING as string}/>
       <body className={`${roboto.variable} ${ibm.variable} font-sans`}> {children} </body>
     </html>
   )
