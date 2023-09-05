ab7980f6b94a0ca66962d74173cba19ea68b8923
Cristofe Albuquerque <cristofealbuquerque7@gmail.com>
2023-09-05 13:42:28 -0300

    fix: google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import {
   IBM_Plex_Mono as IBM
 } from 'next/font/google'
 import GoogleAnalytics from './GoogleAnalytics'
+import Script from 'next/script'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 const ibm = IBM({ subsets: ['latin'], weight: '700', variable: '--font-ibm' })
@@ -24,6 +25,20 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.png" sizes="any" />
 
         <GoogleAnalytics GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_TRAKING as string} />
+
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-JSWRM0N14D`}
+        />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-JSWRM0N14D');
+        `}
+        </Script>
+        
       </head>
       <body className={`${roboto.variable} ${ibm.variable} font-sans`}> {children} </body>
     </html>
